fc3262f4599a5dac23aa14719726851c17d0872f
Commondore <alex.som86@gmail.com>
2025-03-06 21:20:09 +0600

    add env example

--- src/components/order/contact-data/index.tsx
+++ src/components/order/contact-data/index.tsx
@@ -1,20 +1,51 @@
 import { Button } from "@/shared/ui/button";
 import styles from "./style.module.css";
-// import { usePizzaContext } from "@/context/pizza-povider";
+import { useState } from "react";
+import { usePizzaContext } from "@/context/pizza-povider";
 
 export const ContactData = () => {
-  // const { ings, price } = usePizzaContext();
+  const { ings, price } = usePizzaContext();
+  const [customer, setCustomer] = useState({
+    name: "",
+    email: "",
+    address: "",
+    phone: "",
+  });
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCustomer((prevCustomer) => {
+      return {
+        ...prevCustomer,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("send data to firebase");
+  };
+
   return (
     <div className={styles.contactData}>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={styles.group}>
-          <input className={styles.input} type="text" name="name" placeholder="Укажите ваше имя" />
+          <input
+            className={styles.input}
+            type="text"
+            name="name"
+            value={customer.name}
+            onChange={changeHandler}
+            placeholder="Укажите ваше имя"
+          />
         </div>
         <div className={styles.group}>
           <input
             className={styles.input}
             type="email"
             name="email"
+            value={customer.email}
+            onChange={changeHandler}
             placeholder="Укажите ваш email"
           />
         </div>
@@ -23,6 +54,8 @@ export const ContactData = () => {
             className={styles.input}
             type="text"
             name="address"
+            value={customer.address}
+            onChange={changeHandler}
             placeholder="Укажите ваш address"
           />
         </div>
@@ -31,6 +64,8 @@ export const ContactData = () => {
             className={styles.input}
             type="text"
             name="phone"
+            value={customer.phone}
+            onChange={changeHandler}
             placeholder="Укажите ваш phone"
           />
         </div>
